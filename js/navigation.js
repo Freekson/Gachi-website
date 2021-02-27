f3761ee882519b2cfd6ab0bc2f5b187760c29473
Egor <79405864+Freekson@users.noreply.github.com>
2021-02-27 19:35:52 +0100

    Update navigation.js

--- js/navigation.js
+++ js/navigation.js
@@ -1,58 +1,21 @@
 $(document).ready(function() {
     var show_menu = true;
-    $('.main-link').on('click', function(){
-        $('html, body').animate({
-            scrollTop: $('#main').offset().top - 54
-        },700);
-        if(show_menu == false){
-            $('.main-menu').animate({top: -240 }, 700);
-            show_menu = true;
-        }
-    });
-    $('.offer-link').on('click', function(){
-        $('html, body').animate({
-            scrollTop: $('#offer').offset().top -54
-        },700);
-        if(show_menu == false){
-            $('.main-menu').animate({top: -240 }, 700);
-            show_menu = true;
-        }
-    });
-    $('.diet-link').on('click', function(){
-        $('html, body').animate({
-            scrollTop: $('#diet').offset().top - 54
-        },700);
-        if(show_menu == false){
-            $('.main-menu').animate({top: -240 }, 700);
-            show_menu = true;
-        }
-    });
-    $('.contact-link').on('click', function(){
-        $('html, body').animate({
-            scrollTop: $('#contact').offset().top - 54
-        },700);
-        if(show_menu == false){
-            $('.main-menu').animate({top: -240 }, 700);
-            show_menu = true;
-        }
-    });
-    $('.creator-link').on('click', function(){
-        $('html, body').animate({
-            scrollTop: $('#creator').offset().top - 54
-        },700);
-        if(show_menu == false){
-            $('.main-menu').animate({top: -240 }, 700);
-            show_menu = true;
-        }
-    });
     $('.menu-visible').on('click', function(){
         if(show_menu == true){
             $('.main-menu').animate({top: 0 }, 700);
             show_menu = false;
+            $(this).css({
+                'background-color':'black',
+                'color':'whitesmoke'
+            });
         }
         else{
             $('.main-menu').animate({top: -240 }, 700);
             show_menu = true;
+            $(this).css({
+                'background-color':'#fdc501',
+                'color':'black'
+            });
         }
     });
     $(window).resize(function(){
